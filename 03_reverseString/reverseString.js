1ae099543805202926271306869042160a25dd9d
Kurrem <oscar.harmsen@gmail.com>
2024-09-18 18:57:48 +0200

    Assignment 03 - reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    wordLength = word.length-1
+    drow = ''
+    for (let i = wordLength; i >= 0; i--) {
+        drow += word[i]
+    }
+    return drow
 };
 
 // Do not edit below this line
