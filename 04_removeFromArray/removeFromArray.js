4e4f1916442703d62ade71702a36e1b3f22302b1
Kurrem <oscar.harmsen@gmail.com>
2024-09-18 21:28:42 +0200

    Assignment 04, using filter and arguments to find and remove from an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray) { 
+
+    filteredArray = inputArray
+    for (let i = 1; i < arguments.length; i++) {
+        filteredArray = filteredArray.filter((element) => {
+            if (element === arguments[i]) {
+                return false
+            } else return true
+        })
+    }
+
+    return filteredArray
 };
 
 // Do not edit below this line
