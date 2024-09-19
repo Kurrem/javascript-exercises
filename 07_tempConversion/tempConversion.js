55110e92112bd6efce05c5e442c0d00ce3b770bd
Kurrem <oscar.harmsen@gmail.com>
2024-09-19 08:31:25 +0200

    Assignment 07. Two functions for converting temperatures between C and F, rounded to one decimal.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  /*
+  Takes a number input and converts from F to C, rounded to one decimal.
+  Formula: C = (F-32)*5/9 
+  */
+
+ tempCelsius = (tempFahrenheit-32)*5/9
+ return Math.round(tempCelsius*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  /*
+  Takes a number input and converts from F to C, rounded to one decimal.
+  Formula: (9/5)*C+32
+  */
+
+  tempFahrenheit = (9/5)*tempCelsius+32
+  return Math.round(tempFahrenheit*10)/10
 };
 
 // Do not edit below this line
